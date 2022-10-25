4bc7f3e30e1d46fb4c071414a9fcd55e7346ce5f
Amanda Pellerite <amanda.pellerite@pendo.io>
2022-10-24 23:23:12 -0400

    fix: more workflow triggers
    fix(vue): add background color

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
       </div>
     </div>
     <ul class="list-group">
-      <li class="list-group-item" v-for="title in gameTitles" :key="title">{{ title }}</li>
+      <li class="list-group-item" v-for="title in gameTitles" style="background-color: aquamarine" :key="title">{{ title }}</li>
     </ul>
   </div>
 </template>
